5cb8671a56cf00f47a5541c42681ab2acc8e888a
Joshua McKinney <mckinneyj@byui.edu>
2016-12-17 04:08:42 -0700

    added pace line

--- makeChart.js
+++ makeChart.js
@@ -1,5 +1,6 @@
 /* estlint-env browser */
-/* global c3 */
+/* eslint no-console:0, no-unused-vars:0 */
+/* global c3, ss, console */
 
 function fixDataLengths(data) {
     function getLongestLength(longest, current) {
@@ -15,40 +16,83 @@ function fixDataLengths(data) {
 
 
 function fixUpData(data) {
-    function addTotals(item) {
 
-        return item;
+
+
+    function addPaceLines(data) {
+        var keys = Object.keys(data[0]);
+        console.log("keys:", keys);
+
+        function getSumDataSet(item, index) {
+            var sum = 0;
+
+            // loop the keys so that we get all of them
+            keys.forEach(function (key) {
+                sum += item[key];
+            });
+
+            return [index, sum];
+        }
+
+        function addPaceVals() {
+
+        }
+
+        var sumDataSet = data.map(getSumDataSet),
+            lineFun = ss.linearRegressionLine(ss.linearRegression(sumDataSet)),
+            currentY = lineFun(0),
+            x = -1;
+        //plot points untill it crosses x axes
+        while (currentY > 0 && x < 25) {
+            x += 1;
+            currentY = lineFun(x);
+            if (typeof data[x] === "undefined") {
+                data[x] = {};
+            }
+            data[x].Pace = currentY;
+        }
+
+        //console.log(sumDataSet);
 
     }
 
-    function addPaceLine(item) {
+    function convertToC3JSON(data) {
+        var dataObj = {},
+            keys = Object.keys(data[0]);
+        console.log(keys);
 
-
+        //for each key in the first obj get the col that goes with it
+        keys.forEach(function (key) {
+            dataObj[key] = data.map(function (point) {
+                return point[key] || 0;
+            })
+        });
+        return dataObj;
     }
 
-    data = data
-        .map(addTotals)
-        .map(addPaceLine);
-
+    addPaceLines(data);
+    console.log("data:", data);
+    return convertToC3JSON(data);
 }
 
 
-function makeChart(data) {
+function makeChart(dataIn) {
     function makeGroups(data) {
-        var skip = ["pace"];
-        return Object.keys(data).filter(function (name) {
-            return skip.indexOf(name) === -1;
-        });
-
+        var skip = ["Pace"],
+            list = Object.keys(data).filter(function (name) {
+                return skip.indexOf(name) === -1;
+            });
+        console.log("list", list);
+        return list;
     }
 
     var chartSettings = {
         bindto: '#chart',
         data: {
-            json: data,
+            json: dataIn,
             type: 'bar',
             types: {
-                "pace": 'bar'
+                "Pace": 'line'
             },
             groups: [makeGroups(data)]
         },
@@ -81,7 +125,7 @@ var dataStart = [
     },
     {
         Josh: 40,
-        Corey: 240
+        Corey: 40
     },
     {
         Josh: 60,
@@ -89,16 +133,17 @@ var dataStart = [
     },
     {
         Josh: 50,
-        Corey: 220
+        Corey: 20
     }
 ];
 
+var dataEnd = fixUpData(dataStart);
 
-
+console.log("converted:", dataEnd);
 
 var data = {
     "Josh": [30, 20, 50, 40, 60, 50],
     "Corey": [200, 130, 90, 240, 130, 220]
 };
 
-makeChart(data);
+makeChart(dataEnd);
